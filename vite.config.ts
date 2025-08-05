ae635f86e83a44036d9c4c7919ae65d253a824b2
Killershark99999 <killershark@cyberservices.com>
2025-08-05 17:44:35 +0100

    Fix build configuration and paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.lottie'],
   base: '/LatestPortfolio-/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]'
-      }
-    }
-  }
+  assetsInclude: ['**/*.lottie']
 })
